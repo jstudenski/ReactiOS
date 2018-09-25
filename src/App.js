f6f5fdbf294bee5d1a9f831e9495ebd4a5261dce
Jeff Studenski <jstudenski@gmail.com>
2018-09-25 09:27:29 -0500

    test browser specific styles

--- src/App.js
+++ src/App.js
@@ -7,8 +7,46 @@ import Calendar from './components/icons/Calendar.js';
 import Comment from './components/icons/Comment.js';
 import Envelope from './components/icons/Envelope.js';
 
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { footerBackground: 'pink' };
+    this.getBrowser = this.getBrowser.bind(this);
+  }
+  componentDidMount() {
+
+    if((navigator.userAgent.indexOf("Opera") || navigator.userAgent.indexOf('OPR')) != -1 ) {
+      console.log('Opera');
+      } else if(navigator.userAgent.indexOf("Chrome") != -1 ) {
+        this.setState({
+          footerBackground: 'rgb(242, 243, 244)',
+          iconColor: 'rgb(199, 200, 201)',
+        });
+      } else if(navigator.userAgent.indexOf("Safari") != -1) {
+        this.setState({
+          footerBackground: 'rgb(239, 240, 241)',
+          iconColor: 'rgb(213, 214, 215))',
+        });
+      } else if(navigator.userAgent.indexOf("Firefox") != -1 ) {
+      console.log('Firefox');
+      }
+      else if((navigator.userAgent.indexOf("MSIE") != -1 ) || (!!document.documentMode == true )) {//IF IE > 10 
+      console.log('IE');
+      } else {
+      console.log('unknown');
+      }
+
+  }
+  getBrowser = () => {
+
+
+  }
+
+  // getBrowser();
+
   render() {
+
     return (
       <div className="App">
         <div className="container">
@@ -43,8 +81,8 @@ class App extends Component {
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           <br />
 
-          <div className="footer">
-            <div className="footer-icons">
+          <div className="footer" style={{backgroundColor: this.state.footerBackground}}>
+            <div className="footer-icons" style={{color: this.state.iconColor}}>
             <Home /><Ellipsis /><Calendar /><Comment /><Envelope />
             </div>
           </div>
