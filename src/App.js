91c4fdeb519b1c7f63320fb080f968785714ec86
Jeff Studenski <jstudenski@gmail.com>
2018-09-25 06:43:44 -0500

    testing container on mobile

--- src/App.js
+++ src/App.js
@@ -6,15 +6,21 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">iOS IU Kit</h1>
-        </header>
-        <SegmentControl
-          color='rebeccapurple'
-          values={['One', 'Two']}
-          selectedIndex={1}
-        />
-        <div className="footer">test</div>
+        <div className="container">
+          <header className="App-header">
+            <h1 className="App-title">iOS IU Kit</h1>
+          </header>
+          <SegmentControl
+            color='rebeccapurple'
+            values={['One', 'Two']}
+            selectedIndex={1}
+          /><br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <br />
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          <br />
+          <div className="footer">test</div>
+        </div>
       </div>
     );
   }
