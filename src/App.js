bfb735ef7a4bb5733be8a4b1361aaaf8f6ce3ca7
Jeff Studenski <jstudenski@gmail.com>
2018-09-25 08:20:17 -0500

    icon test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import './App.css';
 import SegmentControl from './components/SegmentControl';
+import Home from './components/icons/Home.js';
+import Ellipsis from './components/icons/Ellipsis.js';
 
 class App extends Component {
   render() {
@@ -37,7 +39,10 @@ class App extends Component {
           <br />
           Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
           <br />
-          <div className="footer"></div>
+
+          <div className="footer">
+            <Home /><Ellipsis /><Ellipsis /><Ellipsis />
+          </div>
         </div>
       </div>
     );
