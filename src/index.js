538bdb0320ba8a4b431919e2ce56db3a84eafb92
Jeff Studenski <jstudenski@gmail.com>
2018-09-28 15:37:11 -0500

    adding font sizes

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
+require('typeface-roboto');
+
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
--- src/pages/Ellipsis/Ellipsis.js
+++ src/pages/Ellipsis/Ellipsis.js
@@ -4,6 +4,14 @@ const Ellipsis = () => (
   <div>
     <h2 className="page-heading">Ellipsis</h2>
     <div className="container">...</div>
+    <h1>Large Title</h1>
+    <h2>Title 1</h2>
+    <h3>Title 2</h3>
+    <h4>Title 3</h4>
+    <h5>Headline</h5>
+    <p>Body</p>
+    <h6>Sub Heading</h6>
+
   </div>
 );
 
