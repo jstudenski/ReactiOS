fcf9a2d83da2fbadf0ae12d8fb60adcb2d729bec
Jeff Studenski <jstudenski@gmail.com>
2018-09-28 11:12:46 -0500

    added system colors

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,7 +3,16 @@ import React from 'react';
 const Home = () => (
   <div>
     <h2 className="page-heading">Home</h2>
-    <div className="container">home content</div>
+    <div className="container swatch-grid">
+      <div className="swatch red">Red</div>
+      <div className="swatch orange">Orange</div>
+      <div className="swatch yellow">Yellow</div>
+      <div className="swatch green">Green</div>
+      <div className="swatch teal-blue">Teal Blue</div>
+      <div className="swatch blue">Blue</div>
+      <div className="swatch purple">Purple</div>
+      <div className="swatch pink">Pink</div>
+    </div>
   </div>
 );
 
