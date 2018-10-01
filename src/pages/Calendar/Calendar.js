845ae450adeb998b514a81ad5f8f1e67664406f4
Jeff Studenski <jstudenski@gmail.com>
2018-10-01 15:51:57 -0500

    changed page to pure component

--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
 import Header from '../../components/Header';
 
-class Calendar extends Component {
-  render() {
-    return (
-      <div>
-        <Header
-          title="Calendar"
-          secondary
-          search
-        />
-      </div>
-    );
-  }
-}
+const Calendar = () => (
+  <div>
+    <Header
+      title="Calendar"
+      secondary
+      search
+    />
+  </div>
+);
+
 export default Calendar;
