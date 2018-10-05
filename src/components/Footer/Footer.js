e87fbc81b0b5d6ad409eaab94bd03046f4ba5717
Jeff Studenski <jstudenski@gmail.com>
2018-10-05 15:40:29 -0500

    footer lint

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,64 +5,62 @@ import theme from '../../theme';
 
 const { color } = theme;
 
-class Footer extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-
-  render() {
-    const { backgroundColor, borderColor, pages, activeColor, color, labels } = this.props;
-    const footerStyle = {
-      borderTop: `1px solid ${borderColor}`,
-      backgroundColor: backgroundColor,
-      width: '100%',
-      fontSize: 30,
-      position: 'fixed',
-      bottom: 0,
-      paddingBottom: 10,
-    };
-
-    return (
-      <footer style={footerStyle}>
-        <div className="footer-icons">
-          {pages.map((page, index) => (
-            <NavLink
-              activeStyle={{
-                fontWeight: 'bold',
-                color: activeColor,
-              }}
-              style={{
-                color: color,
-                margin: '0px 10px',
-              }}
-              exact={index === 0}
-              to={page.route}
-            >
-              <div>
-                {page.icon}
-                {labels && <p style={{ fontSize: 10, textAlign: 'center', }}>{page.description}</p>}
-              </div>
-            </NavLink>
-          ))}
-        </div>
-      </footer>
-    );
-  }
-};
+const Footer = ({
+  backgroundColor,
+  borderColor,
+  pages,
+  activeColor,
+  textColor,
+  labels,
+}) => (
+  <footer style={{
+    borderTop: `1px solid ${borderColor}`,
+    backgroundColor,
+    width: '100%',
+    fontSize: 30,
+    position: 'fixed',
+    bottom: 0,
+    paddingBottom: 10,
+  }}
+  >
+    <div className="footer-icons">
+      {pages.map((page, index) => (
+        <NavLink
+          activeStyle={{
+            fontWeight: 'bold',
+            color: activeColor,
+          }}
+          style={{
+            color: textColor,
+            margin: '0px 10px',
+          }}
+          exact={index === 0}
+          to={page.route}
+        >
+          <div>
+            {page.icon}
+            {labels && <p style={{ fontSize: 10, textAlign: 'center' }}>{page.description}</p>}
+          </div>
+        </NavLink>
+      ))}
+    </div>
+  </footer>
+);
 
 Footer.propTypes = {
   activeColor: PropTypes.string,
   borderColor: PropTypes.string,
   backgroundColor: PropTypes.string,
-  color: PropTypes.string,
+  textColor: PropTypes.string,
   labels: PropTypes.bool,
+  pages: PropTypes.instanceOf(Array).isRequired,
 };
 
 Footer.defaultProps = {
   activeColor: color.blue,
   borderColor: color.grey,
   backgroundColor: color.lightGrey,
-  color: color.darkGrey,
+  textColor: color.darkGrey,
   labels: true,
 };
 
