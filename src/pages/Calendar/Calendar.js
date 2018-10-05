1326165f82dbb5f383b8c09cd17712528b09eec6
Jeff Studenski <jstudenski@gmail.com>
2018-10-05 16:35:00 -0500

    working filter test

--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -3,6 +3,40 @@ import React from 'react';
 import Header from '../../components/Header';
 
 class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      initialItems: [
+        'Apples',
+        'Broccoli',
+        'Chicken',
+        'Bacon',
+        'Eggs',
+        'Salmon',
+        'Granola',
+        'Bananas',
+        'Beer',
+        'Wine',
+        'Yogurt',
+      ],
+      items: [],
+    };
+    this.filterList = this.filterList.bind(this);
+  }
+
+  filterList(event) {
+    let updatedList = this.state.initialItems;
+    updatedList = updatedList.filter(function(item){
+      return item.toLowerCase().search(
+        event.target.value.toLowerCase()) !== -1;
+    });
+    this.setState({items: updatedList});
+  }
+
+  componentWillMount (){
+    this.setState({items: this.state.initialItems})
+  }
+
   render() {
     return (
       <div>
@@ -11,64 +45,17 @@ class Calendar extends React.Component {
           secondary
           search
         />
+        <input type="text" placeholder="Search" onChange={this.filterList} />
+        <ul>
+          {
+          this.state.items.map((item) => {
+            return <li key={item}>{item}</li>
+          })
+        }
+        </ul>
       </div>
-    )
+    );
   }
-};
+}
 
 export default Calendar;
-//
-// var FilteredList = React.createClass({
-//   filterList: function(event){
-//     var updatedList = this.state.initialItems;
-//     updatedList = updatedList.filter(function(item){
-//       return item.toLowerCase().search(
-//         event.target.value.toLowerCase()) !== -1;
-//     });
-//     this.setState({items: updatedList});
-//   },
-//   getInitialState: function(){
-//      return {
-//        initialItems: [
-//          "Apples",
-//          "Broccoli",
-//          "Chicken",
-//          "Bacon",
-//          "Eggs",
-//          "Salmon",
-//          "Granola",
-//          "Bananas",
-//          "Beer",
-//          "Wine",
-//          "Yogurt"
-//        ],
-//        items: []
-//      }
-//   },
-//   componentWillMount: function(){
-//     this.setState({items: this.state.initialItems})
-//   },
-//   render: function(){
-//     return (
-//       <div className="filter-list">
-//         <input type="text" placeholder="Search" onChange={this.filterList}/>
-//       <List items={this.state.items}/>
-//       </div>
-//     );
-//   }
-// });
-//
-// var List = React.createClass({
-//   render: function(){
-//     return (
-//       <ul>
-//       {
-//         this.props.items.map(function(item) {
-//           return <li key={item}>{item}</li>
-//         })
-//        }
-//       </ul>
-//     )
-//   }
-// });
-//
