395d494d7244c30613234b08a978cbacde1c9849
jstudenski <jstudenski@gmail.com>
2018-10-06 13:27:37 -0500

    partially working list filter

--- src/pages/Calendar/Calendar.js
+++ src/pages/Calendar/Calendar.js
@@ -1,23 +1,18 @@
 import React from 'react';
 // import PropTypes from 'prop-types';
 import Header from '../../components/Header';
+import List from '../../components/List';
+
 
 class Calendar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       initialItems: [
-        'Apples',
-        'Broccoli',
-        'Chicken',
-        'Bacon',
-        'Eggs',
-        'Salmon',
-        'Granola',
-        'Bananas',
-        'Beer',
-        'Wine',
-        'Yogurt',
+        { title: "Fruit", items: ["A", "B", "C", "D", "E"]},
+        { title: "Vegtables", items: ["A1", "B2", "C3", "D4", "E5"]},
+        { items: ["%A", "X",]},
+        { items: ["@", "#", "^", "@E", "!"]},
       ],
       items: [],
     };
@@ -26,16 +21,28 @@ class Calendar extends React.Component {
 
 
   componentWillMount() {
-    this.setState({ items: this.state.initialItems })
+    this.setState({items: this.state.initialItems})
   }
 
   onChange(event) {
-    let updatedList = this.state.initialItems;
+    let list = this.state.initialItems;
+    let tempArr = [];
+    Object.keys(list).forEach(function(key){
+      list[key].items.forEach(function(item){
+        tempArr.push(item);
+      });
+    })
+    let updatedList = tempArr;
+
     updatedList = updatedList.filter(function(item){
       return item.toLowerCase().search(
-        event.target.value.toLowerCase()) !== -1;
+      event.target.value.toLowerCase()) !== -1;
     });
-    this.setState({items: updatedList});
+    console.log({items: updatedList})
+    console.log(this.state.initialItems);
+    this.setState({
+      items: [{ items: updatedList}]
+    });
   }
 
   render() {
@@ -47,13 +54,7 @@ class Calendar extends React.Component {
           search
           onChange={this.onChange}
         />
-        <ul>
-          {
-          this.state.items.map((item) => {
-            return <li key={item}>{item}</li>
-          })
-        }
-        </ul>
+        <List items={this.state.items} />
       </div>
     );
   }
