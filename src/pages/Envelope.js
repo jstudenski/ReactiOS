c4eda234780e8d076fd6aa9d0f1783e91876ed9a
jstudenski <jstudenski@gmail.com>
2018-10-21 17:47:22 -0500

    new segment control example

--- src/pages/Envelope.js
+++ src/pages/Envelope.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import SegmentControl from '../components/SegmentControl';
 import Header from '../components/Header';
+import Segments from '../components/Segments.js';
 
 const Envelope = () => (
   <div>
     <Header title="Envelope" secondary={false} />
     <div>
       <SegmentControl color="rebeccapurple" values={['One', 'Two']} selectedIndex={1} />
+      <Segments tabs={['None', 'Manual', 'Auto']} color={'#007AFF'} />
     </div>
   </div>
 );
