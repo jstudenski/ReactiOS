22881d8aac89722c43e09fae88bc8e099c7c1eed
jstudenski <jstudenski@gmail.com>
2018-10-25 14:23:04 -0500

    refactoring toggle

--- src/pages/Envelope.js
+++ src/pages/Envelope.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import SegmentControl from '../components/SegmentControl';
 import Header from '../components/Header';
-import Segments from '../components/Segments.js';
+import Segments from '../components/Segments';
 
 const Envelope = () => (
   <div>
     <Header title="Envelope" secondary={false} />
     <div>
       <SegmentControl color="rebeccapurple" values={['One', 'Two']} selectedIndex={1} />
-      <Segments tabs={['None', 'Manual', 'Auto']} color="#007AFF" />
+      <Segments tabs={['Salmon', 'Dark Slate Gray', 'Aquamarine']} color="#007AFF" />
     </div>
   </div>
 );
